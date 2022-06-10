0193a71aeda7df2695a2b2129fb07bb92970d6fa
Shapovalov <shapovalov.anton.111@yandex.ru>
2022-06-10 10:33:16 +0300

    commit

--- The Light/ViewController.swift
+++ The Light/ViewController.swift
@@ -8,9 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    var isLightOn = false
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
+    }
+    
+    fileprivate func updateUI() {
+        view.backgroundColor = isLightOn ? .red : .green
+    }
+    
+    @IBAction func buttonAction(_ sender: UIButton) {
+        isLightOn.toggle()
+        updateUI()
         
     }
 
